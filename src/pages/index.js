67dd9de468f2e47226ef3b97d79e1d2699d69297
Dennis <jungocodes@gmail.com>
2021-02-22 17:41:44 -0500

    Created css folder inside of src folder. Imported style.css in gatsby-browser.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,17 @@ const index = ({data}) => {
     const posts = data.allMdx.nodes
     return (
         <div>
-            <h1>Blog Posts</h1>
+            <h1 >Blog Posts</h1>
             {/* Looping through the post and displaying them on the frontend */}
             {posts.map((post) => {
                 // If there is no title on the post use the slug as the title
                 const title = post.frontmatter.title || post.fields.slug
 
                 return(
-                    <div>
-                        <h1>{title}</h1>
-                        <p>{post.frontmatter.date}</p>
-                        <p>{post.frontmatter.description}</p>
+                    <div  className='card'>
+                        <h1 className='card-title'>{title}</h1>
+                        <p  className='card-date'>{post.frontmatter.date}</p>
+                        <p  className='card-description' >{post.frontmatter.description}</p>
                     </div>
                 )
             })}
